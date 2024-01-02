8c9bb2cfe824738d4ae8a2c2f7faefcd5a4c6980
Alainr701 <60483893+Alainr701@users.noreply.github.com>
2024-01-02 15:29:24 -0400

    fix(1)

---
+++ src/hello_world.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+const char* ssid = "Name_Wifi";
+const char* password = "123456789";
+
+ESP8266WebServer server(80);
+IPAddress local_ip(192, 168, 1, 1);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
+
+void handleRoot() {
+  IPAddress ip = WiFi.localIP();
+  String message = "Hello from " + ip.toString();
+  server.send(200, "text/plain", message);
+}
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.softAP(ssid, password);
+  WiFi.softAPConfig(local_ip, gateway, subnet);
+  Serial.println("Connected to WiFi");
+  Serial.println("Name: " + String(ssid));
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("Server started");
+}
+
+void loop() {
+  server.handleClient();
+}
