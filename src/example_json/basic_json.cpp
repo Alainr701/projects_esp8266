0a68ceae8b7faf87834e3faec74046294d6af083
Alainr701 <60483893+Alainr701@users.noreply.github.com>
2024-01-04 16:33:45 -0400

    feat(3): add example json arduino

---
+++ src/example_json/basic_json.cpp
@@ -0,0 +1,28 @@
+//For example recommended tool: https://arduinojson.org/v6/assistant/
+//This example uses ArduinoJson 6.21.4
+#include <Arduino.h>
+#include <ArduinoJson.h>
+void setup()
+{
+    // ==========>>>>>>>> Serialize JSON
+    StaticJsonDocument<96> doc;
+    doc["id"] = 1123;
+    doc["data"] = "Example Data";
+    String jsonString;
+    serializeJson(doc, jsonString);
+    Serial.println(jsonString);
+
+    // ==========>>>>>>>>Deserialize JSON
+    StaticJsonDocument<96> doc2;
+    String requestBody = "{\"id\": 1123, \"data\": \"Example Data\"}";
+    DeserializationError error = deserializeJson(doc2, requestBody);
+    if (error)
+    {
+        Serial.println("Error al deserializer el JSON");
+        return;
+    }
+}
+
+void loop()
+{
+}
